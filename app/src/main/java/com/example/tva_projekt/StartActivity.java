9e6354d494ac8a2ca422b05f47d07a3ab3cf7893
Julija <julija.pesticek@student.um.si>
2023-05-20 20:49:08 +0200

    Narejen novi screen- start activity.

--- app/src/main/java/com/example/tva_projekt/StartActivity.java
+++ app/src/main/java/com/example/tva_projekt/StartActivity.java
@@ -1,14 +1,27 @@
 package com.example.tva_projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
 import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_activity);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
     }
-}
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+}}
