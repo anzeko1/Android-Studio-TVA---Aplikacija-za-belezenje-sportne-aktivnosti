0500f6825390093ae6459a42de6211480c421b01
anagasperin <anagasperin99@gmail.com>
2023-05-27 15:51:57 +0200

    delujoča prijava in registracija, username se shrani v shared preferences

--- app/src/main/java/com/example/tva_projekt/LoginActivity.java
+++ app/src/main/java/com/example/tva_projekt/LoginActivity.java
@@ -1,30 +1,128 @@
 package com.example.tva_projekt;
 
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-import android.widget.EditText;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
-
-    EditText username = null;
-    EditText password = null;
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
+    private String username;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.editUsername);
+        etPassword = findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.loginButton);
+
+        //Button btnSignUp = findViewById(R.id.registerPage);
+        //Button btnLogout = findViewById(R.id.logoutButton);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
     }
 
-    public void loginInput(View view) {
-        //Pridobitev podatkov iz obrazca
-        username = (EditText) findViewById(R.id.editUsername);
-        password = (EditText) findViewById(R.id.editPassword);
+    private void loginUser() {
+        username = etUsername.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
 
-        //sprememba podatkov v string
-        String stringUsername = username.getText().toString();
-        String stringPassword = password.getText().toString();
+        // Create a JSON object with the login data
+        JSONObject loginData = new JSONObject();
+        try {
+            loginData.put("username", username);
+            loginData.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Send the login data to the server using AsyncTask
+        new LoginTask().execute(loginData.toString());
     }
-}
 
+    private class LoginTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String urlStr = "http://192.168.0.12:3000/user/login";
+
+            try {
+                URL url = new URL(urlStr);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setDoOutput(true);
+
+                // Send the login data to the server
+                OutputStream outputStream = connection.getOutputStream();
+                outputStream.write(params[0].getBytes());
+                outputStream.flush();
+                outputStream.close();
+
+                // Get the response from the server
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // Successful login
+                    // Handle the response here (e.g., parse JSON response, save user data, etc.)
+                    // You can use connection.getInputStream() to read the response body
+                    // For example:
+                    // InputStream inputStream = connection.getInputStream();
+                    // String responseBody = readInputStream(inputStream);
+                    // inputStream.close();
+                    return true;
+                } else {
+                    // Failed login
+                    return false;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d("e", e.getMessage());
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+                if (success) {
+                    saveLoginStatus(true, username);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+
+        private void saveLoginStatus(boolean isLoggedIn, String username) {
+            SharedPreferences sharedPreferences = getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("isLoggedIn", isLoggedIn);
+            editor.putString("username", username);
+            editor.apply();
+
+            Log.d("Shared Preferences", "Username: " + username);
+        }
+    }
--- app/src/main/java/com/example/tva_projekt/RegisterActivity.java
+++ app/src/main/java/com/example/tva_projekt/RegisterActivity.java
@@ -1,39 +1,132 @@
 package com.example.tva_projekt;
 
+import android.content.SharedPreferences;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    EditText username = null;
-    EditText email = null;
-    EditText password = null;
-    EditText gender = null;
-    EditText age = null;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText etUsername, etPassword, etEmail, etGender, etDob;
+    private ToggleButton toggleFemale, toggleMale;
+    private Button btnRegister;
+    private String username;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.editUsername);
+        etPassword = findViewById(R.id.editPassword);
+        etEmail = findViewById(R.id.editEmail);
+        toggleFemale = findViewById(R.id.female);
+        toggleMale = findViewById(R.id.male);
+        etDob = findViewById(R.id.editDob);
+        btnRegister = findViewById(R.id.signInButton);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
     }
 
-    public void loginInput(View view) {
-        //Pridobitev podatkov iz obrazca
-        username = (EditText) findViewById(R.id.editUsername);
-        email = (EditText) findViewById(R.id.editEmail);
-        password = (EditText) findViewById(R.id.editPassword);
+    private void registerUser() {
+        username = etUsername.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        String email = etEmail.getText().toString().trim();
+        String gender = toggleFemale.isChecked() ? "Female" : "Male";
+        int dob = Integer.parseInt(etDob.getText().toString().trim());
 
-        //preberi vrednost vsakega buttona
-        //gender = (EditText) findViewById(R.id.editPassword);
+        // Create a JSON object with the user data
+        JSONObject userData = new JSONObject();
+        try {
+            userData.put("userName", username);
+            userData.put("password", password);
+            userData.put("email", email);
+            userData.put("gender", gender);
+            userData.put("dob", dob);
 
-        age = (EditText) findViewById(R.id.editAge);
+            Log.d("User Data", userData.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-        //sprememba podatkov v string
-        String stringUsername = username.getText().toString();
-        String stringPassword = password.getText().toString();
+        // Send the user data to the server using AsyncTask
+        new RegisterUserTask().execute(userData.toString());
+    }
+
+    private class RegisterUserTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String urlStr = "http://192.168.0.12:3000/user/register";
+
+            try {
+                URL url = new URL(urlStr);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+
+                // Send the JSON data to the server
+                OutputStream outputStream = connection.getOutputStream();
+                outputStream.write(params[0].getBytes());
+                outputStream.flush();
+                outputStream.close();
+
+                // Get the response from the server
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // Registration successful
+                    return true;
+                } else {
+                    // Registration failed
+                    return false;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d("e", e.getMessage());
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (success) {
+                saveLoginStatus(true, username);
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(RegisterActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private void saveLoginStatus(boolean isLoggedIn, String username) {
+            SharedPreferences sharedPreferences = getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("isLoggedIn", isLoggedIn);
+            editor.putString("username", username);
+            editor.apply();
+
+            Log.d("Shared Preferences", "Username: " + username);
+        }
     }
 }
-
